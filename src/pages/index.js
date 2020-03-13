0b60479d0f3e9ffcc633dfd9aa945ba69f3147a6
sagostinelli <batonenbois@gmail.com>
2020-03-13 11:41:52 -0400

    added terminal logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,30 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import GatsbyImage from "../components/gatsbyImage"
+import TerminalImage from "../components/terminalImage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Welcome to your new Gatsby site...on IPFS!</h1>
+    <div
+        style={{
+          maxWidth: `300px`,
+          marginBottom: `1.45rem`,
+          backgroundColor: 'black',
+          padding: '10px',
+          borderRadius: '5px',
+          margin: '0 auto',
+          }}>
+        <TerminalImage />
+      </div>
+      <div style={{ fontSize: '100px', margin: '40px auto', textAlign: 'center' }}>+</div>
+      <div style={{ width: '300px', maxWidth: `300px`, marginBottom: `1.45rem`, margin: '0 auto', }}>
+        <GatsbyImage />
+      </div>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
